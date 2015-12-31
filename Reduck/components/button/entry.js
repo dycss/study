8e6c1447dad872255de9f8785b7ce96cd4d7d62f
donaldcen <donaldcen@tencent.com>
2015-12-31 18:57:44 +0800

    commit

--- Reduck/components/button/entry.js
+++ Reduck/components/button/entry.js
@@ -1,3 +1,4 @@
-var tpl = require('button.ejs');
-//var style = require('./style/button.css');
+var tpl = require('./button.ejs');
+var style = require('./style/button.css');
+document.write(tpl({text: 'test'}));
 console.log(tpl({text: 'test'}));
--- Reduck/webpack.config.js
+++ Reduck/webpack.config.js
@@ -7,8 +7,10 @@ var webpack = require('webpack');
 var glob = require('glob');
 var Path = require('path');
 
+
 //基本路径名字配置
-var componentsPath = '../components/',
+var componentsPath = './components/',
+    outPath = './dist-webpack/components/',
     entryName = 'entry.js';
 
 //计算入口文件路径
@@ -20,24 +22,27 @@ array.forEach(function (filePath) {
     entryMap['entry.' + entryName] = filePath;
 });
 
-
 module.exports = {
-    context: componentsPath,
+    //context: Path.join(__dirname, './'),
     entry: entryMap,
     output: {
-        path: '../dist-webpack/components/',
+        path: outPath,
         filename: '[name].js'
     },
     resolve: {
-        //root: "",
-        //extensions: ['.js', '.css', '.ejs'],
+        //root: Path.join(__dirname, './'),
+        extensions: ['', '.js', '.css', '.ejs'],
         alias: {},
-        modulesDirectories: ['./node_modules', componentsPath + "common", componentsPath + "lib", componentsPath]
+        modulesDirectories: [Path.join(__dirname, 'node_modules'), Path.join(__dirname, componentsPath,"common"), Path.join(__dirname, componentsPath,"lib"), Path.join(__dirname, componentsPath)]
+    },
+    resolveLoader: {
+        //root: Path.join(__dirname, './')
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: "style!css"},
-            {test: /\.ejs/, loader: "ejs"}//"ejs-loader?variable=data"
+            {test: /\.css$/, loader: "style-loader!css-loader"},
+            {test: /\.ejs/, loader: "ejs"},//"ejs-loader?variable=data"
+            {test: /\.jsx?$/, loaders: "jsx?harmony"}
         ]
     }
 };
